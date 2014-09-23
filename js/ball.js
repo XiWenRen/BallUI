79c4970430b709eb36b939d9202fbd1588649dab
XiWenRen <xzhoubingjie@gmail.com>
2014-09-23 21:25:21 +0800

    V0.2
    加入渐变和背景色，有立体感了，但是配色需要改善

--- js/ball.js
+++ js/ball.js
@@ -7,17 +7,12 @@ var seed = (radius * 2) / (25 * 60);
 initBall();
 var usedLength = 0;
 var totalSecends = 0;
-var drarTread = setInterval('drawCenterLine(usedLength += seed)',1000);
+//传入参数要修改，计算方法为开始时间-当前时间 *速度
+var drarTread = setInterval('drawCenterLine(usedLength += seed)',10);
 
 //画一条相对于圆垂直或者水平的直线，把圆分隔成两个部分
 function drawCenterLine(usedLength) {
 	ballCtx.clearRect(0,0,radius * 2,radius * 2);
-	ballCtx.fillText(exchangeSecToMin(totalSecends++),radius,radius);
-//	//设置阴影
-//	ballCtx.shadowOffsetX = 3;
-//	ballCtx.shadowOffsetY = 3;
-//	ballCtx.shadowBlur = 0;
-//	ballCtx.shadowColor = "black";
 	
 	//分割线的长度，以x轴为界分为两半
 	var drawHalfLength = getVerLineOfRightTriangle(usedLength);
@@ -28,11 +23,33 @@ function drawCenterLine(usedLength) {
 	ballCtx.closePath();
 	ballCtx.stroke();
 	//左侧填色
-	ballCtx.fillStyle = 'rgba(255, 0, 0, 0.5)';
-	ballCtx.fillRect(0,0,usedLength,radius * 2);
+//	ballCtx.fillStyle = 'rgba(255, 0, 0, 1)';
+//	ballCtx.fillRect(0,0,usedLength,radius * 2);
 	//右侧填色
-	ballCtx.fillStyle = "rgba(0, 135, 0, 0.5)";
+	ballCtx.fillStyle = "rgba(0, 255, 0, 1)";
 	ballCtx.fillRect(usedLength,0,radius * 2 - usedLength,radius * 2);
+	
+	//绘制渐变
+	var startRadioX = Math.abs(usedLength - (radius * 2)) > 1 ? usedLength : radius;
+//	console.log(startRadioX)
+	var radial = ballCtx.createRadialGradient(startRadioX,radius,0,radius,radius,radius);
+	radial.addColorStop(0,'rgba(255,255,255,0.6)');
+	radial.addColorStop(1,'rgba(80,80,80,0.4)');
+	ballCtx.fillStyle = radial;
+	ballCtx.fillRect(0,0,radius * 2,radius * 2);
+	ballCtx.stroke();
+	
+	radial = ballCtx.createRadialGradient(radius,radius,75,radius,radius,radius);
+	radial.addColorStop(0,'rgba(120,120,120,0.2)');
+	radial.addColorStop(1,'rgba(80,80,80,0.4)');
+	ballCtx.fillStyle = radial;
+	ballCtx.fillRect(0,0,radius * 2,radius * 2);
+	ballCtx.stroke();
+	
+	//显示时间
+	ballCtx.fillText(exchangeSecToMin(totalSecends++),radius,radius);
+	
+	//如果到达圆的最右侧，则停止
 	if(usedLength >= radius * 2){
 		window.clearInterval(drarTread);
 		usedLength = 0;
